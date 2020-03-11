b568e9387ca1b4d4230cb5745b6a866f3849c1de
刘欢 <15804854160@163.com>
2020-03-11 13:21:12 +0800

    插件完成

--- fast-git-code.js
+++ fast-git-code.js
@@ -1,7 +1,14 @@
-// javascript: (function (src) { var srciptEl = document.createElement('script'); srciptEl.setAttribute("src", srciptEl); document.body.appendChild(srciptEl); })("")
-(function (window) {
-  const url = window.location.href;
-  const sourceGraph = 'https://sourcegraph.com/'
-  const last = url.split('://')[1];
-  window.open(sourceGraph + last)
-})(window)
+(function () {
+  const fileNav = document.getElementsByClassName('file-navigation')[0];
+  const btn = document.createElement("span");
+  btn.className = "btn btn-sm btn-blue";
+  btn.innerText = "Source Graph";
+  btn.style.marginLeft = '8px';
+  btn.onclick = function () {
+    const url = window.location.href;
+    const sourceGraph = 'https://sourcegraph.com/'
+    const last = url.split('://')[1];
+    window.open(sourceGraph + last);
+  }
+  fileNav.appendChild(btn);
+})()
